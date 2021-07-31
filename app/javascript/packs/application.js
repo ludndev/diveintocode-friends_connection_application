6fe5f5029fa115c6a4bda44cd240981e6fb4c8fa
ludndev <ludndev@gmail.com>
2021-07-31 18:44:50 +0100

    setup bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,18 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("jquery")
+require("bootstrap")
+
+import "../../assets/stylesheets/application";
+
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
