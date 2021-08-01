5cebdce8362848d0abff995ad054b5328e43db4c
ludndev <ludndev@gmail.com>
2021-08-01 13:07:44 +0100

    session method and middleware

--- app/helpers/auth_helper.rb
+++ app/helpers/auth_helper.rb
@@ -1,2 +1,17 @@
 module AuthHelper
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if !!session[:user_id]
+  end
+
+  def auth_only
+    redirect_to auth_login_path, warning: "You should login first" unless logged_in?
+  end
+
+  def guest_only
+    redirect_to auth_profile_path, warning: "You are already login" if logged_in?
+  end
 end
