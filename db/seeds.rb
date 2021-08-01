3bb21263f61f97aea12f1ab6c476d9515eaf3e9b
ludndev <ludndev@gmail.com>
2021-08-01 18:59:22 +0100

    create default user on dev env migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  User.create(firstname: "John", lastname: "Doe", username: "johndoe", password: "password")
+end
